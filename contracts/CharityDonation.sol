a50bf9ba01ce4cdae74406d0193f16725107c7ee
AYZLFC <almogzamir@gmail.com>
2023-03-11 14:47:26 +0200

    Functions fixes & Clean unuse functions

--- contracts/CharityDonation.sol
+++ contracts/CharityDonation.sol
@@ -5,11 +5,12 @@ contract CharityDonation {
     address payable public contractOwner;
     mapping(address => uint) public donations;
     mapping(address => bool) public charities;
-    mapping(uint => address) private lutcharities;
+    mapping(uint => address) private lutCharities;
     mapping(address => bool) public Donors;
     mapping(uint => address) private lutDonors;
     mapping(address => uint) public totalDonationsByDonor;
     mapping(address => uint) public totalDonationsByCharity;
+    mapping(address => uint) public charitiesMatchedAmount;
     uint public numberOfDonors;
     uint public numberOfCharities;
 
@@ -22,29 +23,64 @@ contract CharityDonation {
         contractOwner = payable(newOwner);
     }
 
-    // function addCharity(address _charity) public returns(address[] memory){
-    //     require(msg.sender == contractOwner, "Only the contract owner can add charities.");
-    //     address[] memory _charit_y = new address[](1);
-    //     charities[_charity] = true;
-    //     _charit_y[1]=_charity;
-    //     return _charit_y;
-    // }
-
     function addCharity(address _charity) public {
         require(msg.sender == contractOwner, "Only the contract owner can add charities.");
         charities[_charity] = true;
         uint charitiesIndex = numberOfCharities++;
-        lutcharities[charitiesIndex]=_charity;
+        lutCharities[charitiesIndex]=_charity;
+        charitiesMatchedAmount[_charity] = 0;
     }
 
+    // The donation function
+    function donate(address _charity) public payable {
+        require(charities[_charity], "Invalid charity address.");
+        require(msg.value > 0, "Donation amount must be greater than 0.");
+        uint matchedAmount = msg.value;
+        charitiesMatchedAmount[_charity] += matchedAmount;
+        if (!Donors[msg.sender]){
+            uint fundersIndex = numberOfDonors++;
+            Donors[msg.sender]=true;
+            lutDonors[fundersIndex]=msg.sender;
+        }
+        donations[_charity] += msg.value;
+        totalDonationsByDonor[msg.sender] += msg.value;
+        totalDonationsByCharity[_charity] += msg.value;
+    }
+
+    //Show all the amount (split by charities) the contract's owner need to match 
+    function getAllCharitiesAndMatchedAmount() public view returns(address[] memory,uint[] memory) {
+        address[] memory _Charities = new address[](numberOfCharities);
+        uint[] memory _MatchedAmounts = new uint[](numberOfCharities);
+        for(uint i=0; i<numberOfCharities; i++){
+            _Charities[i]=lutCharities[i];
+            _MatchedAmounts[i]=charitiesMatchedAmount[lutCharities[i]];
+        }
+        return (_Charities,_MatchedAmounts );
+    }
+
+    //Function for the contract's owner to pay all matched amounts
+    function matchTheDonations() public payable {
+        require(msg.sender == contractOwner, "Only the contract owner can add charities.");
+        uint sumOfMatchedAmount=0;
+        for(uint i=0; i<numberOfCharities; i++){
+            donations[lutCharities[i]] += charitiesMatchedAmount[lutCharities[i]];
+            totalDonationsByCharity[lutCharities[i]] += charitiesMatchedAmount[lutCharities[i]];
+            sumOfMatchedAmount += charitiesMatchedAmount[lutCharities[i]];
+            charitiesMatchedAmount[lutCharities[i]] = 0;
+        }
+        require(msg.value == sumOfMatchedAmount, "The exactly matched amount need to be deliverd.");
+    }
+
+    //Show all charities
     function getAllCharities() public view returns(address[] memory) {
         address[] memory _Charities = new address[](numberOfCharities);
         for(uint i=0; i<numberOfCharities; i++){
-            _Charities[i]=lutcharities[i];
+            _Charities[i]=lutCharities[i];
         }
         return _Charities;
     }
     
+    //Shows all donors
     function getAllDonors() public view returns(address[] memory) {
         address[] memory _Donors = new address[](numberOfDonors);
         for(uint i=0; i<numberOfDonors; i++){
@@ -53,49 +89,47 @@ contract CharityDonation {
         return _Donors;
     }
 
-
-    function donate(address _charity) public payable {
-        require(charities[_charity], "Invalid charity address.");
-        require(msg.value > 0, "Donation amount must be greater than 0.");
-        uint matchedAmount = msg.value;
-        if (contractOwner.balance >= msg.value) {
-            contractOwner.transfer(msg.value);
-        } else {
-            matchedAmount = contractOwner.balance;
-            contractOwner.transfer(matchedAmount);
+    //Shows all donors and their donations
+    function getAllDonorsAndDonations() public view returns(address[] memory,uint[] memory) {
+        address[] memory _Donors = new address[](numberOfDonors);
+        uint[] memory _Donations = new uint[](numberOfDonors);
+        for(uint i=0; i<numberOfDonors; i++){
+            _Donors[i]=lutDonors[i];
+            _Donations[i]=totalDonationsByDonor[lutDonors[i]];
         }
-        if (!Donors[msg.sender]){
-            uint fundersIndex = numberOfDonors++;
-            Donors[msg.sender]=true;
-            lutDonors[fundersIndex]=msg.sender;
-        }
-        donations[_charity] += msg.value + matchedAmount;
-        totalDonationsByDonor[msg.sender] += msg.value; // בדיקה לגבי האם התורם  קיים, צריך לבדוק מה קורה עם תורם חדש? האם הוא נוסף לרשימה?
-        totalDonationsByCharity[_charity] += msg.value + matchedAmount;
+        return (_Donors,_Donations );
     }
 
+    //Shows all charities and their current donation balance
+    function getAllCharitiesAndDonations() public view returns(address[] memory,uint[] memory) {
+        address[] memory _Charities = new address[](numberOfCharities);
+        uint[] memory _Donations = new uint[](numberOfCharities);
+        for(uint i=0; i<numberOfCharities; i++){
+            _Charities[i]=lutCharities[i];
+            _Donations[i]=donations[lutCharities[i]];
+        }
+        return (_Charities,_Donations);
+    }
 
+    //Shows all charities and their total donation balance ever
+     function getAllCharitiesAndTotalDonationsByCharity() public view returns(address[] memory,uint[] memory) {
+        address[] memory _Charities = new address[](numberOfCharities);
+        uint[] memory _TotalDonationsByCharity = new uint[](numberOfCharities);
+        for(uint i=0; i<numberOfCharities; i++){
+            _Charities[i]=lutCharities[i];
+            _TotalDonationsByCharity[i]=totalDonationsByCharity[lutCharities[i]];
+        }
+        return (_Charities,_TotalDonationsByCharity);
+    }
+
+    //Withdraw function for the charities to withdraw all their current donations
     function withdraw() public {
+        require(charities[msg.sender] = true, "You are not one of the charities.");
         uint amount = donations[msg.sender];
         require(amount > 0, "No funds available for withdrawal.");
         donations[msg.sender] = 0;
         payable(msg.sender).transfer(amount);
     }
 
-//     function getAllDonorsAndDonations() public view returns (address[] memory, uint256[] memory) {
-//     address[] memory donorAddresses = new address[](totalDonationsByDonor.length);
-//     uint256[] memory donorDonations = new uint256[](totalDonationsByDonor.length);
-//     uint256 i = 0;
-//     for (address donorAddress ; totalDonationsByDonor) {
-//         donorAddresses[i] = donorAddress;
-//         donorDonations[i] = totalDonationsByDonor[donorAddress];
-//         i++;
-//     }
-//     return (donorAddresses, donorDonations);
-// }
 
-
-    // function autoMatchDonation() public payable {
-    //     require(msg.sender == contractOwner, "Only the contract owner can auto-match donations.");
-    // }
 }
